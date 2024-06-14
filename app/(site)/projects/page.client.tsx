b6af75bded7655dce4dbcfc9fb26c697d2ae632d
Richard Lopez <rsl01dw@gmail.com>
2024-06-13 18:28:22 -0700

    added suspense to project block

--- app/(site)/projects/page.client.tsx
+++ app/(site)/projects/page.client.tsx
@@ -12,29 +12,14 @@ import { urlFor } from "@/sanity/sanity-utils";
 import Image from "next/image";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 
-export function ProjectClient({ projects }: { projects: Project[] }) {
+function ProjectBlock({ projects }: { projects: Project[] }) {
   const searchParams = useSearchParams();
   const view = searchParams.get("view");
   const [isGrid, useIsGrid] = useState<boolean>(view === "grid");
-
   return (
     <>
-      <GridContainer>
-        <div className="col-span-full flex items-start relative">
-          <TextReveal
-            className="text-8xl lg:text-9xl uppercase"
-            text={"All Works"}
-            as="h1"
-          />
-          <span className="text-xl absolute right-0 top-0 lg:static">
-            [ {projects.length} ]
-          </span>
-        </div>
-      </GridContainer>
-      <HR title={"Works"} />
-
       <div className="flex w-full justify-end px-3 lg:px-5">
         <div className="flex items-center gap-x-5 ml-auto">
           <Link href={{ pathname: "/projects", query: { view: "list" } }}>
@@ -62,6 +47,34 @@ export function ProjectClient({ projects }: { projects: Project[] }) {
       ) : (
         <ProjectArchiveGrid projects={projects} />
       )}
+    </>
+  );
+}
+
+export function ProjectClient({ projects }: { projects: Project[] }) {
+  const searchParams = useSearchParams();
+  const view = searchParams.get("view");
+  const [isGrid, useIsGrid] = useState<boolean>(view === "grid");
+
+  return (
+    <>
+      <GridContainer>
+        <div className="col-span-full flex items-start relative">
+          <TextReveal
+            className="text-8xl lg:text-9xl uppercase"
+            text={"All Works"}
+            as="h1"
+          />
+          <span className="text-xl absolute right-0 top-0 lg:static">
+            [ {projects.length} ]
+          </span>
+        </div>
+      </GridContainer>
+      <HR title={"Works"} />
+
+      <Suspense>
+        <ProjectBlock projects={projects} />
+      </Suspense>
     </>
   );
 }
